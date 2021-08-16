241d303deb33821f0fc40189270c7efbb9c980e4
RKania96 <kaniaradoslaw7@gmail.com>
2021-08-16 17:17:14 +0200

    floating obliczenia

--- Core/Inc/Settings.h
+++ Core/Inc/Settings.h
@@ -10,7 +10,7 @@
 
 //Client settings
 #define DEVICE_NAME "DeviceNo1"
-#define L_USER		"User1"
+#define L_USER		"user1"
 #define L_PASSWORD	"password"
 
 //Topics
@@ -19,8 +19,6 @@
 //WiFi credentials
 #define WIFI_SSID "UPC16FF1FD"
 #define WIFI_PASS "CCe54nuknuvu"
-//#define WIFI_SSID "Zhone_AB41"
-//#define WIFI_PASS "znid314265153"
 
 //Connection settings
 #define NETWORK_HOST "192.168.0.111"
